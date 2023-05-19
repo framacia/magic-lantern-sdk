f74627bab2ab1ab1d0243e56713c35a708316d7e
rmunozor2703 <rena.munozo@gmail.com>
2023-05-19 13:58:30 +0200

    frequency changes in image msg

--- Assets/ImageDisplay.cs
+++ Assets/ImageDisplay.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
-using RosFrame = RosMessageTypes.MagicLantern.FrameMsg;
+using RosFrame = RosMessageTypes.MagicLantern.FrameCompressedMsg;
 using Unity.Robotics.ROSTCPConnector.ROSGeometry;
 
 
 public class ImageDisplay : MonoBehaviour
 {
-    public GameObject image;
+    public GameObject quad;
 
     void Start()
     {
@@ -17,7 +17,11 @@ public class ImageDisplay : MonoBehaviour
 
     void ShowImage(RosFrame imageMessage)
     { 
-        print("the width of the image is: " + imageMessage.width); 
-        
+
+      // Create a texture. Texture size does not matter, since
+        // LoadImage will replace with with incoming image size.
+        Texture2D tex = new Texture2D(320, 240);
+        tex.LoadImage(imageMessage.data);
+        GetComponent<Renderer>().material.mainTexture = tex;
     }
 }
