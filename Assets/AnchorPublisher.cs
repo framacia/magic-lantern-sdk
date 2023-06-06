ee651a6e709767cbcd9766d94900978aed659efd
rmunozor2703 <rena.munozo@gmail.com>
2023-06-06 14:16:44 +0200

    changes in the anchor publisher

--- Assets/AnchorPublisher.cs
+++ Assets/AnchorPublisher.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.Robotics.ROSTCPConnector;
 using RosMessageTypes.MagicLantern;
@@ -8,53 +6,58 @@ public class AnchorPublisher : MonoBehaviour
 {
     ROSConnection ros;
     public string topicName = "anchor_info";
+    public float publishMessageFrequency = 1.0f;
 
+    // Used to determine how much time has elapsed since the last message was published
+    private float timeElapsed;
+    
     void Start()
     {
+
         ros = ROSConnection.GetOrCreateInstance();
         ros.RegisterPublisher<AnchorInformationMsg>(topicName);
-        CollectAnchorData();
+       
     }
 
-    void CollectAnchorData()
+    private void Update()
     {   
+        
+        timeElapsed += Time.deltaTime;
 
+        if (timeElapsed > publishMessageFrequency)
+        {
+            GameObject[] anchorObjects = GameObject.FindGameObjectsWithTag("anchor");
+            foreach (GameObject anchorObject in anchorObjects)
+            {
+            
+                AnchorDefinition anchorDefinition = anchorObject.GetComponent<AnchorDefinition>();
 
-    GameObject[] anchorObjects = GameObject.FindGameObjectsWithTag("anchor");
+                // Create AnchorInfo object and populate the data
+                AnchorInformationMsg anchorInfo = new AnchorInformationMsg();
+                anchorInfo.id = int.Parse(anchorDefinition.anchorId);
+                anchorInfo.location[0] = anchorObject.transform.position.x;
+                anchorInfo.location[1] = anchorObject.transform.position.y;
+                anchorInfo.location[2] = anchorObject.transform.position.z;
+                anchorInfo.location[3] = anchorObject.transform.rotation.eulerAngles.x;
+                anchorInfo.location[4] = anchorObject.transform.rotation.eulerAngles.y;
+                anchorInfo.location[5] = anchorObject.transform.rotation.eulerAngles.z;
+                anchorInfo.volume_size = anchorDefinition.volumeSize;
+                anchorInfo.filter_proximity = anchorDefinition.filterProximity;
 
-    foreach (GameObject anchorObject in anchorObjects)
-    {
-    AnchorDefinition anchorDefinition = anchorObject.GetComponent<AnchorDefinition>();
+                // Finally send the message to server_endpoint.py running in ROS
+                ros.Publish(topicName, anchorInfo);
 
-    // Create AnchorInfo object and populate the data
-    AnchorInformationMsg anchorInfo = new AnchorInformationMsg();
-    anchorInfo.id = int.Parse(anchorDefinition.anchorId);
-    anchorInfo.location[0] = anchorObject.transform.position.x;
-    anchorInfo.location[1] = anchorObject.transform.position.y;
-    anchorInfo.location[2] = anchorObject.transform.position.z;
-    anchorInfo.location[3] = anchorObject.transform.rotation.eulerAngles.x;
-    anchorInfo.location[4] = anchorObject.transform.rotation.eulerAngles.y;
-    anchorInfo.location[5] = anchorObject.transform.rotation.eulerAngles.z;
-    anchorInfo.volume_size = anchorDefinition.volumeSize;
-    anchorInfo.filter_proximity = anchorDefinition.filterProximity;
-
-    
-    PublishAnchorInfo(anchorInfo);
-    
-    // Finally send the message to server_endpoint.py running in ROS
-    ros.Publish(topicName, anchorInfo);
-
+                //PublishAnchorInfo(anchorInfo);
+                timeElapsed = 0;
+            }
+        }
     }
 
-    
-    
-    }
-
-    void PublishAnchorInfo(AnchorInformationMsg anchorInfo)
+    private void PublishAnchorInfo(AnchorInformationMsg anchorInfo)
     {
         // Publish the anchor information
         Debug.Log("Anchor ID: " + anchorInfo.id);
-        Debug.Log("Location: " + string.Join(", ", anchorInfo.location));
+        Debug.Log("Location:s " + string.Join(", ", anchorInfo.location));
         Debug.Log("Volume Size: " + anchorInfo.volume_size);
         Debug.Log("Filter Proximity: " + anchorInfo.filter_proximity);
     }
