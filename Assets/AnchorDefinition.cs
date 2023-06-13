deb28adde4bfd0d74f543e1b6112bc52f732025e
Paul Hine <paul@hine.works>
2023-06-13 18:22:16 +0200

    anchor viz

--- Assets/AnchorDefinition.cs
+++ Assets/AnchorDefinition.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AnchorDefinition : MonoBehaviour
@@ -7,16 +5,44 @@ public class AnchorDefinition : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (_dirty)
+        {
+
+            _dirty = false;
+        }
     }
+
+    [SerializeField, SerializeProperty("filterProximity")]
+    private float _filterProximity;
+
+    private bool _dirty = true;
 
     public string anchorId;
     public float volumeSize;
-    public float filterProximity;
+
+    public float filterProximity
+    {
+        get
+        {
+            return _filterProximity;
+        }
+        set
+        {
+            Debug.Log("Set filterProximity");
+            _filterProximity = value;
+            BoundaryCylinder.transform.localScale = new Vector3(
+                _filterProximity,
+                BoundaryCylinder.transform.localScale.y,
+                _filterProximity
+            );
+            _dirty = true;
+        }
+    }
+
+    public GameObject BoundaryCylinder;
 }
