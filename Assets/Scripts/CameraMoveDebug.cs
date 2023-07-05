be3c8e0931ceec9124c0e615eadea8b7634b8b36
Fran Macía <frankmmacia@gmail.com>
2023-07-05 16:20:51 +0200

    Updated to unity 2022.3.4, working AR Foundation implementation

--- Assets/Scripts/CameraMoveDebug.cs
+++ Assets/Scripts/CameraMoveDebug.cs
@@ -5,15 +5,11 @@ using UnityEngine;
 
 public class CameraMoveDebug : MonoBehaviour
 {
-    Camera cam;
     [SerializeField] float speed = 1f;
     [SerializeField] TextMeshProUGUI fpsCounterText;
 
     private void Start()
     {
-
-
-        cam = GetComponent<Camera>();
 
         Invoke("DelayedStart", 0.5f);
     }
@@ -29,31 +25,33 @@ public class CameraMoveDebug : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+#if UNITY_EDITOR
         float xAxisValue = Input.GetAxis("HorizontalArrow");
         float zAxisValue = Input.GetAxis("VerticalArrow");
 
-        if (cam != null)
+        if (gameObject != null)
         {
             //Rotation
-            cam.transform.Rotate(-zAxisValue * speed, xAxisValue * speed, 0);
+            gameObject.transform.Rotate(-zAxisValue * speed, xAxisValue * speed, 0);
 
             //Force Z rotation to 0
             Vector3 currentRotation = transform.rotation.eulerAngles;
             transform.rotation = Quaternion.Euler(currentRotation.x, currentRotation.y, 0);
 
             //Translation
-            Vector3 movementVector = (cam.transform.forward * Input.GetAxis("Vertical") * 10) + (cam.transform.right * Input.GetAxis("Horizontal") * 10);
+            Vector3 movementVector = (gameObject.transform.forward * Input.GetAxis("Vertical") * 10) + (gameObject.transform.right * Input.GetAxis("Horizontal") * 10);
 
-            cam.transform.position += new Vector3(movementVector.x, 0, movementVector.z) * Time.deltaTime;
+            gameObject.transform.position += new Vector3(movementVector.x, 0, movementVector.z) * Time.deltaTime;
         }
+#endif
 
     }
 
     private void Update()
     {
-        if (fpsCounterText != null)
-        {
-            fpsCounterText.text = (1.0f / Time.deltaTime).ToString("F1");
-        }
+        //if (fpsCounterText != null)
+        //{
+        //    fpsCounterText.text = (1.0f / Time.deltaTime).ToString("F1");
+        //}
     }
 }
