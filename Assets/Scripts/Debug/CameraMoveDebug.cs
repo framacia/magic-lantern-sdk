76c8114039f2ebdb260c0e39f7e8c80287c9bff7
Fran Macía <frankmmacia@gmail.com>
2023-07-25 15:08:41 +0200

    Working on terrain optimization

--- Assets/Scripts/Debug/CameraMoveDebug.cs
+++ Assets/Scripts/Debug/CameraMoveDebug.cs
@@ -18,7 +18,7 @@ public class CameraMoveDebug : MonoBehaviour
         //Is it really necessary setting this??? Find a good place to add it 
         QualitySettings.vSyncCount = 0;
 
-        Application.targetFrameRate = 61;
+        Application.targetFrameRate = 120;
     }
 
     // Update is called once per frame
