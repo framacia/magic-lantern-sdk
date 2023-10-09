6910821f86535364ad304dde94d97a35b1d74fbd
Fran Macía <frankmmacia@gmail.com>
2023-10-09 11:22:25 +0200

    Dog AI Look rotation limit

--- Assets/Scripts/IKLook.cs
+++ Assets/Scripts/IKLook.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Security.Permissions;
+using UnityEngine;
 
 public class IKLook : MonoBehaviour
 {
@@ -10,8 +11,9 @@ public class IKLook : MonoBehaviour
     //public Vector3 StartDirection;
     private Quaternion startBoneRotation;
     private Vector3 startParentRotation;
+    private float previousBoneTransformDiffY;
 
-    void Awake()
+    void Start()
     {
         if (target == null)
             return;
@@ -26,33 +28,39 @@ public class IKLook : MonoBehaviour
         if (target == null)
             return;
 
-        float prevBoneRotX = bone.eulerAngles.x;
-        float prevBoneRotY = bone.eulerAngles.y;
-        float prevBoneRotZ = bone.eulerAngles.z;
-
         Vector3 direction = (target.position - bone.position).normalized;
         Vector3 lookRotation = (Quaternion.LookRotation(direction) * startBoneRotation).eulerAngles;
 
+        //Account for initial rotation
         bone.eulerAngles = lookRotation - startParentRotation;
-        if (Mathf.Abs((bone.localEulerAngles.x + 360) % 360 - (startBoneRotation.eulerAngles.x + 360) % 360) > rotationLimit.x)
-        {
-            print("local x " + (bone.localEulerAngles.x + 360) % 360);
-            //bone.eulerAngles = new Vector3(prevBoneRotX, bone.eulerAngles.y, bone.eulerAngles.z);
-            bone.eulerAngles = new Vector3(bone.eulerAngles.x, prevBoneRotY, bone.eulerAngles.z);
-        }
-        //if (Mathf.Abs((bone.localEulerAngles.y + 360) % 360 - (startBoneRotation.eulerAngles.y + 360) % 360) > rotationLimit.y)
+        //Remap to invert X axis
+        bone.eulerAngles = new Vector3(-bone.eulerAngles.x, bone.eulerAngles.y, bone.eulerAngles.z);
+
+        float currentBoneTransformDiffY = ((bone.eulerAngles.y - transform.localEulerAngles.y - 180) + 360) % 360;
+        //float currentBoneTransformDiffY = bone.eulerAngles.y - transform.localEulerAngles.y;
+
+        //if (currentBoneTransformDiffY > 190 || currentBoneTransformDiffY < 0)
         //{
-        //    bone.eulerAngles = new Vector3(bone.eulerAngles.x, prevBoneRotY, bone.eulerAngles.z);
+        //    bone.eulerAngles = new Vector3(bone.eulerAngles.x, previousBoneTransformDiffY, bone.eulerAngles.z);
+        //    //bone.eulerAngles = previousBoneRotation;
         //}
-        //if (Mathf.Abs((bone.localEulerAngles.z + 360) % 360 - (startBoneRotation.eulerAngles.z + 360) % 360) > rotationLimit.z)
+        //else //If rotation is within acceptable range, overwrite previousBoneTransformDiffY
         //{
-        //    bone.eulerAngles = new Vector3(bone.eulerAngles.x, bone.eulerAngles.y, prevBoneRotZ);
+        //    previousBoneTransformDiffY = currentBoneTransformDiffY;
         //}
 
-        //bone.localEulerAngles = new Vector3(
-        //    Mathf.Clamp(bone.localEulerAngles.x, -360, rotationLimit.x),
-        //    Mathf.Clamp(bone.localEulerAngles.y, -rotationLimit.y, rotationLimit.y),
-        //    Mathf.Clamp(bone.localEulerAngles.z, -rotationLimit.z, rotationLimit.z));
+        if (bone.localEulerAngles.x > 30 || bone.localEulerAngles.x < -30)
+        {
+            bone.eulerAngles = new Vector3(bone.eulerAngles.x, previousBoneTransformDiffY, bone.eulerAngles.z);
+        }
+        else
+        {
+            previousBoneTransformDiffY = currentBoneTransformDiffY;
+        }
+
+        print("prev: " + previousBoneTransformDiffY);
+        print("bone: " + bone.eulerAngles.y);
+        print("diff: " + currentBoneTransformDiffY);
     }
 }
 
