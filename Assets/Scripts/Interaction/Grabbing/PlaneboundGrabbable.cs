da6e6faa095fe65db4f6f46bf1a019cdd9516a81
Francisco Macia <frankmmacia@gmail.com>
2023-10-10 11:08:00 +0200

    changes before emil demo

--- Assets/Scripts/Interaction/Grabbing/PlaneboundGrabbable.cs
+++ Assets/Scripts/Interaction/Grabbing/PlaneboundGrabbable.cs
@@ -12,33 +12,40 @@ public class PlaneboundGrabbable : Grabbable
         base.Start();
         constraintPlane = new Plane(this.transform.forward, this.transform.position);
     }
-    public override void Grab(){
+    public override void Grab()
+    {
         rb.useGravity = false;
         rb.isKinematic = false;
         base.Grab();
     }
 
-    public override void Release(){
+    public override void Release()
+    {
         rb.useGravity = usesGravity;
         base.Release();
     }
 
-    public override void UpdateGrabbedPosition(){
+    public override void UpdateGrabbedPosition()
+    {
         Ray ray = new Ray(cam.transform.position, CalculateCameraToTargetVector());
         float distance;
-        if(constraintPlane.Raycast(ray, out distance)){
+        if (constraintPlane.Raycast(ray, out distance))
+        {
             Vector3 targetPosition = cam.transform.position + ray.direction * distance;
             this.rb.MovePosition(Vector3.Lerp(this.rb.position, targetPosition, Time.fixedDeltaTime * lerpScale));
         }
     }
 
-    protected virtual void FixedUpdate(){
-        if(state == GrabbableState.FREE){
+    protected virtual void FixedUpdate()
+    {
+        if (state == GrabbableState.FREE)
+        {
             CalculatePlaneboudPosition();
         }
     }
 
-    protected void CalculatePlaneboudPosition(){
+    protected void CalculatePlaneboudPosition()
+    {
         //Calculate distance from the plane and project it to the normal to apply the correction
         Vector3 correction = Vector3.Project(this.transform.position - (constraintPlane.distance * -constraintPlane.normal), constraintPlane.normal);
         this.rb.MovePosition(rb.position - correction);
